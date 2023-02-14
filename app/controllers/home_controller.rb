6c62a8b5f31ccfbc064c9f2c9f6a9d1b3b05207f
rordev <rordev@ongraph.com>
2023-02-14 15:58:55 +0530

    added stripe payment with 3d secure

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,35 +1,35 @@
 class HomeController < ApplicationController
   def create_subscription
+
     begin
       current_user.setup_stripe_card_details(params[:stripeToken])
 
-      Stripe::Charge.create(
+      payment_create = Stripe::PaymentIntent.create(
         amount: (User::PLAN_AMOUNT.to_i * 100),
-        currency: 'GBP',
-        customer: current_user.stripe_customer_id
+        currency: 'usd',
+        description: 'Software development services',
+        customer: current_user.stripe_customer_id,
+        shipping: {
+          name: 'Jenny Rosen',
+          address: {
+            line1: '510 Townsend St',
+            postal_code: '98140',
+            city: 'San Francisco',
+            state: 'CA',
+            country: 'US',
+          },
+        },
+        payment_method_types: ['card'],
+        payment_method_options: {"card":{"request_three_d_secure":"any"}},
+        confirm: true      )
+
+      payment_confirm = Stripe::PaymentIntent.confirm(
+        "#{payment_create.id}",
+        {return_url: "#{create_cardless_url}"},
+        {payment_method: 'pm_card_visa'}
       )
 
-      if current_user.go_cardless_mandate.present? && current_user.go_cardless_customer.present?
-
-        create_gocardless_subscription
-
-        flash[:success] = 'Your payment has been successfully completed and subscription added to existing bank account.'
-        return redirect_to root_path
-      else
-        success_redirect_url = "#{root_url}/confirm_mandate"
-
-        redirect_flow = GO_CARDLESS_CLIENT.redirect_flows.create(
-          params: {
-            description: 'Demo GoCardless',
-            session_token: "#{current_user.id}",
-            success_redirect_url: success_redirect_url,
-            prefilled_customer: {
-              email: "#{current_user.email}"
-            }
-          }
-        )
-        return redirect_to redirect_flow.redirect_url
-      end
+      redirect_to "#{payment_confirm.next_action.redirect_to_url.url}"
 
     rescue Exception => e
       flash[:error] = e.message
@@ -37,7 +37,32 @@ class HomeController < ApplicationController
     end
   end
 
+  def create_cardless
+    if current_user.go_cardless_mandate.present? && current_user.go_cardless_customer.present?
+
+        create_gocardless_subscription
+
+        flash[:success] = 'Your payment has been successfully completed and subscription added to existing bank account.'
+        return redirect_to root_path
+    else
+      success_redirect_url = "#{root_url}/confirm_mandate"
+
+      redirect_flow = GO_CARDLESS_CLIENT.redirect_flows.create(
+        params: {
+          description: 'Demo GoCardless',
+          session_token: "#{current_user.id}",
+          success_redirect_url: success_redirect_url,
+          prefilled_customer: {
+            email: "#{current_user.email}"
+          }
+        }
+      )
+      return redirect_to redirect_flow.redirect_url
+    end
+  end
+
   def confirm_mandate
+
     begin
       redirect_flow = GO_CARDLESS_CLIENT.redirect_flows.complete(
                         params[:redirect_flow_id],
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # get 'home/index'
   root 'home#index'
   post '/create_subscription', to: 'home#create_subscription'
+  get '/create_cardless', to: 'home#create_cardless'
   get '/confirm_mandate' => 'home#confirm_mandate', as: 'confirm_mandate'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
