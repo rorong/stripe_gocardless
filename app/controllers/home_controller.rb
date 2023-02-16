66865e2850cf32a090cfd69c42de4b6452a298f1
rordev <rordev@ongraph.com>
2023-02-16 15:19:15 +0530

    fixed indentation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
-class HomeController < ApplicationController
-  #create a stripe payment with API
-  def create_subscription
+# frozen_string_literal: true
 
+# Description/Explanation of HomeController
+class HomeController < ApplicationController
+  # create a stripe payment with API
+  def create_subscription
     begin
       current_user.setup_stripe_card_details(params[:stripeToken])
       payment_create = Stripe::PaymentIntent.create(payment_params)
@@ -10,7 +12,6 @@ class HomeController < ApplicationController
         {return_url: "#{create_cardless_url}"},
         {payment_method: User::PAYMENT_METHOD}
       )
-
       redirect_to "#{payment_confirm.next_action.redirect_to_url.url}"
 
     rescue Exception => e
@@ -19,15 +20,13 @@ class HomeController < ApplicationController
     end
   end
 
-  #create a GoCardless subscription for card holder
+  # create a GoCardless subscription for card holder
   def create_cardless
     if current_user.go_cardless_mandate.present? && current_user.go_cardless_customer.present?
-        create_gocardless_subscription
-
-        flash[:success] = 'Your payment has been successfully completed and subscription added to existing bank account.'
-        redirect_to root_path
+      create_gocardless_subscription
+      flash[:success] = 'Your payment has been successfully completed and subscription added to existing bank account.'
+      redirect_to root_path
     else
-
       redirect_flow = GO_CARDLESS_CLIENT.redirect_flows.create(
         gocardless_redirect_params
       )
@@ -35,85 +34,79 @@ class HomeController < ApplicationController
     end
   end
 
-  #confirming the GoCardless for user
+  # confirming the GoCardless for user
   def confirm_mandate
-
     begin
       redirect_flow = GO_CARDLESS_CLIENT.redirect_flows.complete(
-                        params[:redirect_flow_id],
-                        params: { session_token: "#{current_user.id}" }
-                      )
+        params[:redirect_flow_id],
+        params: { session_token: "#{current_user.id}" }
+      )
       current_user.go_cardless_customer = redirect_flow.links.customer
       current_user.go_cardless_mandate = redirect_flow.links.mandate
 
       if current_user.save(validate: false)
-
         create_gocardless_subscription
-
         flash[:notice] = 'Your payment has been successfully completed and subscription added to your GoCardless bank account.'
       else
         flash[:error] = 'Something went wrong!'
       end
-
     rescue StandardError => e
       flash[:error] = e
     end
-
     redirect_to root_path
   end
 
+  private
 
- private
-    # payment's params
-    def payment_params
-      {
-       amount: (User::PLAN_AMOUNT.to_i * 100),
-        currency: User::CURRENCY,
-        description: User::DESCRIPTION,
-        customer: current_user.stripe_customer_id,
-        shipping: {
-          name: User::SHIPPING[:name],
-          address: {
-            line1: User::SHIPPING[:address][:line1],
-            postal_code: User::SHIPPING[:address][:postal_code],
-            city: User::SHIPPING[:address][:city],
-            state: User::SHIPPING[:address][:state],
-            country: User::SHIPPING[:address][:country],
-          },
-        },
-        payment_method_types: ['card'],
-        payment_method_options: {"card":{"request_three_d_secure":"any"}},
-        confirm: true
-      }
-    end
-
-    # redirect params of gocardless
-    def gocardless_redirect_params
-      {params: {
-          description: 'Demo GoCardless',
-          session_token: "#{current_user.id}",
-          success_redirect_url: "#{root_url}/confirm_mandate",
-          prefilled_customer: {
-            email: "#{current_user.email}"
-          }
-        }}
-    end
-
-    #create a gocardless subscription
-    def create_gocardless_subscription
-      GO_CARDLESS_CLIENT.subscriptions.create(
-        params: {
-          amount: User::PLAN_AMOUNT.to_i,
-          currency: User::GO_CARDLESS_CURRENCY,
-          name: User::GO_CARDLESS_NAME,
-          interval_unit: User::INTERVAL_UNIT,
-          interval: 1,
-          start_date: Date.current + 2.month,
-          day_of_month: 1,
-          links: {
-            mandate: current_user.go_cardless_mandate
-          }
+  # payment's params
+  def payment_params
+    { amount: (User::PLAN_AMOUNT.to_i * 100),
+      currency: User::CURRENCY,
+      description: User::DESCRIPTION,
+      customer: current_user.stripe_customer_id,
+      shipping: {
+        name: User::SHIPPING[:name],
+        address: {
+          line1: User::SHIPPING[:address][:line1],
+          postal_code: User::SHIPPING[:address][:postal_code],
+          city: User::SHIPPING[:address][:city],
+          state: User::SHIPPING[:address][:state],
+          country: User::SHIPPING[:address][:country]
         }
-      )
-    end
+      },
+      payment_method_types: ['card'],
+      payment_method_options: {"card":{"request_three_d_secure":"any"}},
+      confirm: true
+    }
+  end
+
+  # redirect params of gocardless
+  def gocardless_redirect_params
+    { params: {
+      description: 'Demo GoCardless',
+      session_token: "#{current_user.id}",
+      success_redirect_url: "#{root_url}/confirm_mandate",
+      prefilled_customer: {
+        email: "#{current_user.email}"
+      }
+    } }
+  end
+
+  # create a gocardless subscription
+  def create_gocardless_subscription
+    GO_CARDLESS_CLIENT.subscriptions.create(
+      params: {
+        amount: User::PLAN_AMOUNT.to_i,
+        currency: User::GO_CARDLESS_CURRENCY,
+        name: User::GO_CARDLESS_NAME,
+        interval_unit: User::INTERVAL_UNIT,
+        interval: 1,
+        start_date: Date.current + 2.month,
+        day_of_month: 1,
+        links: {
+          mandate: current_user.go_cardless_mandate
+        }
+      }
+    )
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,39 +1,43 @@
+# frozen_string_literal: true
+
+# Description/Explanation of User class
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  PLAN_AMOUNT = '100'.freeze
-  CURRENCY = 'usd'.freeze
-  DESCRIPTION = 'Software development services'.freeze
+  PLAN_AMOUNT = '100'
+  CURRENCY = 'usd'
+  DESCRIPTION = 'Software development services'
   SHIPPING = {
-    name: 'Jenny Rosen'.freeze,
+    name: 'Jenny Rosen',
     address: {
-              line1: '510 Townsend St'.freeze,
-              postal_code: '98140'.freeze,
-              city: 'San Francisco'.freeze,
-              state: 'CA'.freeze,
-              country: 'US'.freeze,
-            }}
-  PAYMENT_METHOD = 'pm_card_visa'.freeze
-  INTERVAL_UNIT = 'monthly'.freeze
-  GO_CARDLESS_NAME = 'Test Monthly Plan'.freeze
-  GO_CARDLESS_CURRENCY = 'GBP'.freeze
+      line1: '510 Townsend St',
+      postal_code: '98140',
+      city: 'San Francisco',
+      state: 'CA',
+      country: 'US'
+    }
+  }.freeze
+  PAYMENT_METHOD = 'pm_card_visa'
+  INTERVAL_UNIT = 'monthly'
+  GO_CARDLESS_NAME = 'Test Monthly Plan'
+  GO_CARDLESS_CURRENCY = 'GBP'
 
   def setup_stripe_card_details(stripe_token)
     if stripe_customer_id.blank?
       customer = Stripe::Customer.create(
-                  email: email,
-                  description: "Customer for #{email}",
-                  source: stripe_token # obtained with Stripe.js
-                )
+        email: email,
+        description: "Customer for #{email}",
+        source: stripe_token # obtained with Stripe.js
+      )
       self.update_attribute(:stripe_customer_id, customer.id)
     else
       Stripe::Customer.update(stripe_customer_id,
-      {
-        source: stripe_token
-      })
+                              {
+                                source: stripe_token
+                              })
     end
   end
 end
